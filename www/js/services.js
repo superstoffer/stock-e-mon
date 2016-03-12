07e8aefe3a41b0630ad2617ad72f669b1e189e55
= <wisbech@gmail.com>
2016-03-12 11:59:51 +0100

    perfomance field added to user

--- www/js/services.js
+++ www/js/services.js
@@ -3,12 +3,20 @@ angular.module('services', [])
 .factory('Data', function Data() {
 	// Service logic
 	// ...
+	var performance = {
+		wins: [],
+		earnings: 10,
+		prospect: 0.5, // value [0:1.]
+		prospect_earnings: 100, // historic value of earnings of succesful trades
+	}
+
 	var user = {
 			name: "you",
 			email: "u@u.dkdkdkd",
 			balance: 100.1,
 		    open_trades: [ 'la', 'lal', 'ala' ],
-		    closed_trades: [ 'udd', 'ud' , 'uddd']
+		    closed_trades: [ 'udd', 'ud' , 'uddd'],
+		    performance: performance
 	};
 
 	var settings = {}
